a98f4228f809989fd61b3a18aefc72d315e9c98a
giacatano <142597978+giacatano@users.noreply.github.com>
2024-03-07 09:08:27 +0200

    created project

---
+++ MyApp2/Homework-Assignment-SevenB/Homework-Assignment-SevenB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Homework-Assignment-SevenB
+//
+//  Created by Gia Catano on 2024/03/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var greenScreenLabel: UILabel!
+    
+    @IBAction func pressMeButton(_ sender: Any) {
+        greenScreenLabel.text = "It's still green"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
